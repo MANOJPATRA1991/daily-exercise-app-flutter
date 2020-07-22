32180a42d0d7065193ba06ee86b74dc3335126e5
Manoj Kumar Patra <patra.manoj0@gmail.com>
2020-07-23 01:07:59 +0530

    Define color constants

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const COLORS = {
+  'BACKGROUND': Color(0xFFF8F8F8),
+  'ACTIVE_ICON': Color(0xFFE68342),
+  'TEXT': Color(0xFF222B45),
+  'BLUE_LIGHT': Color(0xFFC7B8F5),
+  'BLUE': Color(0xFF817DC0),
+  'SHADOW': Color(0xFFE6E6E6),
+};
